9372b3d08929f4d89c4982d6181adf00d3488c80
caroline <carolinepedroso85@gmail.com>
2025-02-03 11:14:35 -0300

    att

--- Dia-01/MaiorNumero.js
+++ Dia-01/MaiorNumero.js
@@ -1,9 +1,10 @@
+//Logica - Maior numero entre dois numeros
 const n1 = 9
 const n2 = 6
 
 if (n1>n2) {
-    console.log(n1+"é o maior");
+    console.log(n1 + " é o maior");
     
 } else{
-    console.log(n2+"é o maior");
+    console.log(n2 + " é o maior");
 }
