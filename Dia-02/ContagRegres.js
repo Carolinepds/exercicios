5cb016fb17f0b44c3f77c2a58d43cf197995e0ac
caroline <carolinepedroso85@gmail.com>
2025-02-04 11:15:09 -0300

    att

--- Dia-02/ContagRegres.js
+++ Dia-02/ContagRegres.js
@@ -1,7 +1,7 @@
-let n1 =10
+    let n1 =10
 
-do {
-    console.log(`valor de n1: ${n1}`);
-    n1--
-}while(n1>0)
+    do {
+        console.log(`valor de n1: ${n1}`);
+        n1--
+    }while(n1>0)
 
